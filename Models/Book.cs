ab16bc80f134492d63be7b3216f8e5a81ab0ec56
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-08-03 14:56:31 +0400

    last update

--- Models/Book.cs
+++ Models/Book.cs
@@ -9,7 +9,7 @@ namespace SimpleLibraryManagement.Models
     // This class represents a book in the library system
     public class Book 
     {
-        public int Id { get; private set; }  // Unique identifier for each book
+        public int BookId { get; private set; }  // Unique identifier for each book
         public string Title  { get; set; }  // Title of the book
         public string Author  { get; set; }   // Author of the book
         public bool IsAvailable { get; set; } // Indicates whether the book is currently available for borrowing
