bb7dd1db7c6a2113b8c743b9d634337483beac25
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-08-03 15:36:10 +0400

    Unique identifier for the borrow record

--- Models/BorrowRecord.cs
+++ Models/BorrowRecord.cs
@@ -9,7 +9,7 @@ namespace SimpleLibraryManagement.Models
     // This class represents a record of a book being borrowed by a member
     public class BorrowRecord
     {
-        public int BRId { get; set; }
+        public int BRId { get; set; }   // Unique identifier for the borrow record
         public int MemberId { get; set; } 
         public int BookId { get; set; }
         public DateTime BorrowDate { get; set; }
