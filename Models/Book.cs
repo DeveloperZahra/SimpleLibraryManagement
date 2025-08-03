3ca1b1e58089f0aaef9928e8eb84211ba6b6b289
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-08-03 14:45:39 +0400

    This class represents a book in the library system

--- Models/Book.cs
+++ Models/Book.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace SimpleLibraryManagement.Models
 {
-    internal class Book
+    // This class represents a book in the library system
+    public class Book 
     {
+        public int Id { get; set; }
+        public string Title  { get; set; }
+        public string Author  { get; set; }
+        public bool IsAvailable { get; set; }
     }
 }
