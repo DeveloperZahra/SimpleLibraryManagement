af48b85c307fe2c4fd3e35eb044dadb5f640e443
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-08-03 14:52:48 +0400

    Constructor to initialize the member with ID and Name

--- Models/Member.cs
+++ Models/Member.cs
@@ -11,5 +11,12 @@ namespace SimpleLibraryManagement.Models
     {
         public int Id { get; private set; } // Unique identifier for each member (set privately, read-only from outside)
         public string Name { get;  set; }    // Name of the member
+
+        // Constructor to initialize the member with ID and Name
+        public Member(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
     }
 }
