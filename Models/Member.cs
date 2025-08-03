58c39ec14e90b5aba4b07a3343c4f52c56a89ef5
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-08-03 14:57:15 +0400

    last update

--- Models/Member.cs
+++ Models/Member.cs
@@ -9,13 +9,13 @@ namespace SimpleLibraryManagement.Models
     // This class represents a library member
     public class Member
     {
-        public int Id { get; private set; } // Unique identifier for each member (set privately, read-only from outside)
+        public int MemberId { get; private set; } // Unique identifier for each member (set privately, read-only from outside)
         public string Name { get;  set; }    // Name of the member
 
         // Constructor to initialize the member with ID and Name
         public Member(int id, string name)
         {
-            Id = id;
+            MemberId = id;
             Name = name;
         }
     }
