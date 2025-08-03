7f095b288b3a0b6da31766f1eb80066d3aa09d27
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-08-03 14:54:31 +0400

    I changed the class to public

--- Models/Member.cs
+++ Models/Member.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace SimpleLibraryManagement.Models
 {
     // This class represents a library member
-    internal class Member
+    public class Member
     {
         public int Id { get; private set; } // Unique identifier for each member (set privately, read-only from outside)
         public string Name { get;  set; }    // Name of the member
