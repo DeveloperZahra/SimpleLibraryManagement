9a4387f31d0a699731d5d0022aafe33f44117cce
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-08-03 14:45:57 +0400

    Unique identifier for each book

--- Models/Book.cs
+++ Models/Book.cs
@@ -9,7 +9,7 @@ namespace SimpleLibraryManagement.Models
     // This class represents a book in the library system
     public class Book 
     {
-        public int Id { get; set; }
+        public int Id { get; set; }  // Unique identifier for each book
         public string Title  { get; set; }
         public string Author  { get; set; }
         public bool IsAvailable { get; set; }
