b74857b5575c0b30973d2293b06e0ab9aca951be
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-08-04 15:38:25 +0400

    Create an instance of the LibraryService, injecting the repositories through its constructor

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace SimpleLibraryManagement
             IBookRepository bookRepository = new BookRepository(); // Create an instance of the BookRepository using the IBookRepository interface
             IMemberRepository memberRepository = new MemberRepository(); // Create an instance of the MemberRepository using the IMemberRepository interface
             IBorrowRecordRepository borrowRecordRepository = new BorrowRecordRepository(); // Create an instance of the BorrowRecordRepository using the IBorrowRecordRepository interface
-            ILibraryService libraryService = new LibraryService(bookRepository, memberRepository, borrowRecordRepository); 
+            ILibraryService libraryService = new LibraryService(bookRepository, memberRepository, borrowRecordRepository); // Create an instance of the LibraryService, injecting the repositories through its constructor  
 
 
 
