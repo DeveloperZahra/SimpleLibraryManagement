96d1cbd23cc3b11302efc0d624e8189520afd9ce
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-08-04 14:22:02 +0400

    Path to the file where book data will be stored or loaded from

--- FileContext.cs
+++ FileContext.cs
@@ -12,7 +12,7 @@ namespace SimpleLibraryManagement
     // Static class responsible for handling file operations such as saving and loading data
     public static class FileContext
     {
-        private static string BookFilePath = "books.json";
+        private static string BookFilePath = "books.json"; // Path to the file where book data will be stored or loaded from
         private static string MemberFilePath = "members.json";
         private static string BorrowRecordFilePath = "BorrowRecords.json";
 
