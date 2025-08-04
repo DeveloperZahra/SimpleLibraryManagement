39f187acb8301331644dc5b76dc10b9aca77d04f
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-08-04 15:36:58 +0400

    Create an instance of the MemberRepository using the IMemberRepository interface

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace SimpleLibraryManagement
         static void Main(string[] args)
         {
             IBookRepository bookRepository = new BookRepository(); // Create an instance of the BookRepository using the IBookRepository interface
-            IMemberRepository memberRepository = new MemberRepository();
+            IMemberRepository memberRepository = new MemberRepository(); // Create an instance of the MemberRepository using the IMemberRepository interface
             IBorrowRecordRepository borrowRecordRepository = new BorrowRecordRepository();
             ILibraryService libraryService = new LibraryService(bookRepository, memberRepository, borrowRecordRepository); 
 
