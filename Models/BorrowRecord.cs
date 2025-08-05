add11e64dd9c0669c0ced61b8f411afd793e4ace
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-08-05 15:03:33 +0400

    Static variable to keep track of the total number of borrow records created

--- Models/BorrowRecord.cs
+++ Models/BorrowRecord.cs
@@ -14,6 +14,12 @@ namespace SimpleLibraryManagement.Models
         public int BookId { get; set; }     // ID of the book being borrowed
         public DateTime BorrowDate { get; set; }    // Date when the book was borrowed
         public DateTime ReturnDate { get; set; } // Expected return date of the borrowed book
+        public static int BorrowRecordCount = 0; // Static variable to keep track of the total number of borrow records created
+        public BorrowRecord()
+        {
+            BorrowRecordCount++;
+            BRId = BorrowRecordCount;
+        }
 
 
     }
